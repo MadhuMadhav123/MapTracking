d9083fb65bcc2418c1a1e59bf7e65813c8425b88
MadhuMadhav123 <madhavaraometta@gmail.com>
2020-01-13 12:30:52 +0530

    removed comment

--- app/src/main/java/com/example/maptracking/activities/MainActiviy.java
+++ app/src/main/java/com/example/maptracking/activities/MainActiviy.java
@@ -40,7 +40,7 @@ public class MainActiviy extends AppCompatActivity {
     String audioSavePath;
     int selectedPosition;
     MediaPlayer mediaPlayer;
-    MediaRecorder mediaRecorder; //Added comment madhu
+    MediaRecorder mediaRecorder;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
